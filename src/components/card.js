1943197c1e08ed087d23fc1c0308354680599ed1
landonwjohnson <landonwjohnson@gmail.com>
2017-10-24 16:58:26 -0600

    fixed name header

--- src/components/card.js
+++ src/components/card.js
@@ -2,30 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { getPokemon } from '../services/pokemon';
 import '../css/card.css';
+import { getCharacteristics } from '../services/pokemon';
 
 export default class Card extends React.Component {
   constructor(props){
     super(props)
       this.state = {
         pokemon: {},
-        id: 3
+        id: 25
       }
-    
+
   }
+
+
 
   componentDidMount(){
     getPokemon(this.state.id).then(pokemon => {
       this.setState({
         pokemon: pokemon
       })
-    })
+  })
+
   }
   render() {
     return (
       <div className="pokemon-card-container">
             <div className="pokemon-title">
             <h1 className="pokemon-name">{this.state.pokemon.name} </h1>
-            <h1 className="pokemon-id">{this.state.pokemon.id} </h1>
+            <h1 className="pokemon-id">#{this.state.pokemon.id} </h1>
             </div>
 
             <div className="picture-frame">
@@ -33,10 +37,10 @@ export default class Card extends React.Component {
             </div>
             
        
-         
+            <input pokemonCount={this.state.id} type="number" value={this.state.id}/>         
             <h1>{this.state.pokemon.height} {this.state.pokemon.weight}</h1>
             
-            <h1>{this.state.pokemon.type}</h1>
+
         
       </div>
     );
--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -5,3 +5,9 @@ export const getPokemon = function(pokemonID) {
     return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
     .then(res => res.json())
 }
+
+export const getCharacteristics = function(pokemonID) {
+    return fetch(`https://pokeapi.co/api/v2/characteristic/${pokemonID}`)
+    .then(res => res.json())
+}
+
